4922403f386a0ddd6b871dd32f22aecd8a560c42
jbPark <whdqja918@gmail.com>
2024-09-05 18:00:35 +0900

    mutations 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <CompletedTodo/>
-    <AddTodo @add-todo="addTodo"/>
+    <CompletedTodo />
+    <AddTodo />
     <hr>
-    <TodoList 
-      @toggle-checkbox="toggleCheckBox"
-      @click-delete="onClickDelete"
-    />
+    <TodoList />
     {{  todoItems }}
   </div>
 </template>
@@ -34,32 +31,31 @@ export default {
   },
 
   methods: {
-    addTodo(text){
-      this.todoItems.push({
-        id: Math.random(),
-        text: text,
-        checked: false,
-      })
-      this.todoText = '';
-    },
+    // addTodo(text){
+    //   this.todoItems.push({
+    //     id: Math.random(),
+    //     text: text,
+    //     checked: false,
+    //   })
+    //   this.todoText = '';
+    // },
 
-    toggleCheckBox({id, checked}){
-      console.log(id, checked);
-      const index = this.todoItems.findIndex(todo => {
-        return todo.id === id;
-      })
-      this.todoItems[index].checked = checked;
-    },
-
-    onClickDelete(id){
-      // if(confirm("삭제하시겠습니까?")){
-      //   const index = this.todoItems.findIndex(todo => {
-      //     return todo.id === id;
-      //   });
-      //   this.todoItems.splice(index, 1);
-      // }
-      this.todoItems = this.todoItems.filter(todo => todo.id !== id);
-    }
+    // toggleCheckBox({id, checked}){
+    //   console.log(id, checked);
+    //   const index = this.todoItems.findIndex(todo => {
+    //     return todo.id === id;
+    //   })
+    //   this.todoItems[index].checked = checked;
+    // },
+    // onClickDelete(id){
+    //   // if(confirm("삭제하시겠습니까?")){
+    //   //   const index = this.todoItems.findIndex(todo => {
+    //   //     return todo.id === id;
+    //   //   });
+    //   //   this.todoItems.splice(index, 1);
+    //   // }
+    //   this.todoItems = this.todoItems.filter(todo => todo.id !== id);
+    // }
   }
 }
 </script>
